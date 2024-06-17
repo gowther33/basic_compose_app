f7bf28cbe54aa036dbcda05dfad26c294432375b
hanan <hananjohn111@gmail.com>
2024-06-17 18:38:07 +0500

    card added

--- app/src/main/java/com/example/compose_playground/MainActivity.kt
+++ app/src/main/java/com/example/compose_playground/MainActivity.kt
@@ -1,29 +1,61 @@
 package com.example.compose_playground
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_playground.ui.theme.Compose_playgroundTheme
+import org.jetbrains.annotations.Contract
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Compose_playgroundTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    TopAppBar(
+                        title = { /*TODO*/ },
                         modifier = Modifier.padding(innerPadding)
                     )
+                    Playground()
                 }
             }
         }
@@ -31,17 +63,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Playground(){
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary)
+    ) {
+        MySurface()
+        CustomText()
+    }
+}
+
+@Composable
+fun MySurface(){
+    Surface(
+        modifier = Modifier.padding(16.dp),
+        border = BorderStroke(2.dp, Color.Red),
+        contentColor = Color.Blue,
+        tonalElevation = 8.dp,
+        shape = CircleShape
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.girl),
+            contentDescription = null,
+            modifier = Modifier
+                .size(100.dp)
+        )
+    }
+}
+
+@Composable
+fun CustomText(){
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        "Hello World",
+        modifier = Modifier
+            .background(Color.Green)
+            .padding(16.dp)
+            .width(200.dp),
+        textAlign = TextAlign.Left
     )
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_playgroundTheme {
-        Greeting("Android")
+        Playground()
     }
 }
